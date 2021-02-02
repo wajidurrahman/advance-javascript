b2177bfb7e8dc9032e23aa1b23daf1a69a9e6314
Wajidur Rahman <wrfuad0@gmail.com>
2021-02-02 17:03:12 +0600

    null change output line

--- null-vs-undefined.js
+++ null-vs-undefined.js
@@ -1,44 +1,45 @@
-// let pakhi;
-// console.log(pakhi);
+let pakhi;
+console.log(pakhi);
 
-//output:- undefined
+// output:- undefined
 
-// function add (num1, num2){
-//       console.log(num1 + num2);
-// }
-// const result = add(13, 82);
-// console.log(result);
+function add (num1, num2){
+      console.log(num1 + num2);
+}
+const result = add(13, 82);
+console.log(result);
 
 // output :- undefined
 
-// function add (num1, num2){
-//       console.log(num1 + num2);
-//       return
-// }
-// const result = add(13, 82);
-// console.log(result);
+function add (num1, num2){
+      console.log(num1 + num2);
+      return
+}
+const result = add(13, 82);
+console.log(result);
 
 // output :-undefined
 
-// function add (num1, num2){
-//       console.log(num1, num2);
-//       return
-// }
-// const result = add(13);
-// console.log(result);
+function add (num1, num2){
+      console.log(num1, num2);
+      return
+}
+const result = add(13);
+console.log(result);
 
-// output undefined
+// output:- undefined
 
-// const premik = {name:"smart dude", phone:458421};
-// console.log(premik.gf);
+const premik = {name:"smart dude", phone:458421};
+console.log(premik.gf);
 
-// undefined
+//output:- undefined
 
-// let fun = undefined;
-// console.log(fun);
+let fun = undefined;
+console.log(fun);
 
-// undefined
+//output:- undefined
 
 let ages = [2, 5, 9]
 console.log(ages[11]);
 
+//  output:- undefined
