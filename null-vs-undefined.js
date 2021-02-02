27bc6499ef0c3cc9dc1a3dd8cd4bcd4bdd0c9576
Wajidur Rahman <wrfuad0@gmail.com>
2021-02-02 17:00:20 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,44 @@
+// let pakhi;
+// console.log(pakhi);
+
+//output:- undefined
+
+// function add (num1, num2){
+//       console.log(num1 + num2);
+// }
+// const result = add(13, 82);
+// console.log(result);
+
+// output :- undefined
+
+// function add (num1, num2){
+//       console.log(num1 + num2);
+//       return
+// }
+// const result = add(13, 82);
+// console.log(result);
+
+// output :-undefined
+
+// function add (num1, num2){
+//       console.log(num1, num2);
+//       return
+// }
+// const result = add(13);
+// console.log(result);
+
+// output undefined
+
+// const premik = {name:"smart dude", phone:458421};
+// console.log(premik.gf);
+
+// undefined
+
+// let fun = undefined;
+// console.log(fun);
+
+// undefined
+
+let ages = [2, 5, 9]
+console.log(ages[11]);
+
