8883952244fc6cd4b041af25508fd539375f9045
Wajidur Rahman <wrfuad0@gmail.com>
2021-02-03 00:33:40 +0600

    onek error deklam

---
+++ scope.js
@@ -0,0 +1,18 @@
+let bonus = 20;
+
+function sum (first, second){
+    let result = first + second + bonus;
+    // console.log(bonus);
+    if(result > 9){
+        var mood = "happy";
+            mood = "funky";
+            mood = "cranky";
+        // console.log(mood);
+    }
+    return result;
+}
+const output = sum(3, 7);
+// console.log(bonus);
+// console.log(output);
+
+//  const let scope thatjsdude.com
